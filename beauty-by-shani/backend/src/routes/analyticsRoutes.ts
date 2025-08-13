0c1c799c51d9b5d9e0e9c9c48d6f0c78d5ef8407
Yehoshua Cohen <100349784+Shuicohen@users.noreply.github.com>
2025-08-13 20:27:13 +0300

    Delete beauty-by-shani directory

--- beauty-by-shani/backend/src/routes/analyticsRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAnalytics } from '../controllers/analyticsController';
-import { authMiddleware } from '../middlewares/authMiddleware';
-
-const router = Router();
-
-router.get('/', authMiddleware, getAnalytics);
-
-export default router;
